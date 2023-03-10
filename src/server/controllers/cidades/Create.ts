f720a4d8d1d6e8d1d06b3485938fb5909387b220
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-09 23:01:18 -0300

    feat: Validation of body, query, params, and headers with YUP and express.

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
-  console.log(req.body.nome);
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined;
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
+  console.log(validateData);
 
   return res.send('Create!');
 };
